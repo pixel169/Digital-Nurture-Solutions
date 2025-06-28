9d9e26a645507d0e7743187e6d7e5a78531bdfbe
Priyanshu Ranjan <priyanshtan@gmail.com>
2025-06-28 23:40:21 +0530

    Added moq files

--- week2-AdvSQL_NUnit/NUnit and Moq/1. Moq-Handson/CustomerComm.Tests/CustomerCommTests.cs
+++ week2-AdvSQL_NUnit/NUnit and Moq/1. Moq-Handson/CustomerComm.Tests/CustomerCommTests.cs
@@ -1,7 +1,6 @@
-﻿// Import the necessary libraries
-using Moq;
+﻿using Moq;
 using NUnit.Framework;
-using CustomerCommLib; // Your own project with the classes to test
+using CustomerCommLib; 
 
 namespace CustomerComm.Tests
 {
@@ -11,26 +10,13 @@ namespace CustomerComm.Tests
         [Test]
         public void SendMailToCustomer_ShouldReturnTrue()
         {
-            // ARRANGE
             var mockMailSender = new Mock<IMailSender>();
 
             mockMailSender.Setup(ms => ms.SendMail(It.IsAny<string>(), It.IsAny<string>()))
                           .Returns(true);
-
-            // FIX 1: Use the fully qualified name to avoid namespace collision.
-            // We are creating an instance of the CustomerComm class from the CustomerCommLib namespace.
             var customerComm = new CustomerCommLib.CustomerComm(mockMailSender.Object);
-
-            // ACT
             bool result = customerComm.SendMailToCustomer();
-
-            // ASSERT
-
-            // FIX 2: Use the modern NUnit "Constraint Model" for assertions.
-            // This is the recommended syntax.
             Assert.That(result, Is.True);
-
-            // This verification call remains the same and is still good practice.
             mockMailSender.Verify(ms => ms.SendMail(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
         }
     }
--- week2-AdvSQL_NUnit/NUnit and Moq/1. Moq-Handson/CustomerCommLib/CustomerComm.cs
+++ week2-AdvSQL_NUnit/NUnit and Moq/1. Moq-Handson/CustomerCommLib/CustomerComm.cs
@@ -2,12 +2,7 @@ namespace CustomerCommLib
 {
     public class CustomerComm
     {
-        // This holds our dependency. It can be a REAL MailSender
-        // or a MOCK MailSender.
         private readonly IMailSender _mailSender;
-
-        // This is Constructor Injection.
-        // We are "injecting" the mail sender dependency from the outside.
         public CustomerComm(IMailSender mailSender)
         {
             _mailSender = mailSender;
@@ -15,12 +10,8 @@ namespace CustomerCommLib
 
         public bool SendMailToCustomer()
         {
-            // Actual application logic would go here to determine
-            // the customer's address and the message.
-            string email = "cust123@abc.com";
-            string message = "Some Message";
-
-            // We then use the injected dependency to do the work.
+            string email = "sample mail";
+            string message = "Heil Cognizanr lords";
             return _mailSender.SendMail(email, message);
         }
     }
