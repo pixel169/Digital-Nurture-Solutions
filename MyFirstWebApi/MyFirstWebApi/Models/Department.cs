1036a9dd4f297c2c5437ac0a79a31a90fe8abc1a
PRIYANSHU RANJAN <63237679+pixel169@users.noreply.github.com>
2025-07-13 22:39:17 +0530

    Delete MyFirstWebApi directory

--- MyFirstWebApi/MyFirstWebApi/Models/Department.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyFirstWebApi.Models
-{
-    public class Department
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- MyFirstWebApi/MyFirstWebApi/Models/Skill.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyFirstWebApi.Models
-{
-    public class Skill
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
